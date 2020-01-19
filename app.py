dd90668b5a293fe4c10890e6d6142858ed7464a0
Rachit Pareek <rachitpareek@berkeley.edu>
2020-01-19 13:54:18 -0800

    updated port

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from application import app, db
 
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    # Bind to PORT if defined, otherwise default to 8080.
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
 
